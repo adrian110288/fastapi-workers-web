257416eb550f7aa2dbf54c80474fb0d24f45418f
Adrian Lesniak <adrian110288@protonmail.com>
2025-02-07 18:54:09 +0000

    FIrst commit

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [input, setInput] = useState<string>("")
+  const [results, setResults] = useState<{
+    results: string[],
+    duration: number
+  }>()
+
+  useEffect(() => {
+
+    if (!input) return setResults(undefined)
+    
+    const fetchData = async () => {
+        const response = await fetch(`/api/search?query=${input}`)
+        // setResults(response)
+    }
+
+    fetchData()
+
+  }, [input])
+
+  return(
+    <div>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+    </div>
+  );
+}
